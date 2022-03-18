4ebd1825e5691dbf075f11add366fda63ce8dd63
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-18 10:57:19 -0400

    Initial Commit

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+/* define your functions here */
+
+function calculateTotal(quantity, price){
+    return quantity*price;
+}
+
+function outputCarRow(item, total){
+    let result = 0;
+    result.toFixed();
+    document.write(result);
+}
+
+
+
+
+
+
+        
