5be7d83333fa48c88decc1cb0460f8d188439aba
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-22 20:40:19 -0400

    Done? before todo

--- js/functions.js
+++ js/functions.js
@@ -8,8 +8,28 @@ function outputCartRow(item, total){
     document.write(item.title + " " +item.price+ " "+item.filename);
 }
 
-function d(){
-    loop();
+function createContent(row, text){
+    const celElement = document.createElement("td");
+    celElement.textContent = text;
+    row.appendChild(celElement);
+}
+
+function addRow(tbody, classType, span, text, quantity, bold){
+    const row = document.createElement("tr");
+    //row.classList.add(classType);
+    const cell = document.createElement("td");
+    cell.textContent = text;
+    cell.colSpan = span;
+    row.appendChild(cell);
+    const cell1 = document.createElement("td");
+    cell1.textContent = moneyFormat(quantity);
+    row.appendChild(cell1);
+    bold ? cell1.classList.add("focus"): "";
+    tbody.append(row);
+}
+
+function moneyFormat(number){
+    return '$'+(number.toFixed(2));
 }
 
 
--- js/lab8_problem1.js
+++ js/lab8_problem1.js
@@ -1,19 +1,42 @@
-
 const tax_rate = .10; //prompt('Enter tax rate (0.10)');
-const shipping_threshold = 10   ; //= prompt('Enter shipping threshold (1000)');
-//TODO: change back to prompt
+const shipping_threshold = 300; //prompt('Enter shipping threshold (1000)');
+//TODO: activate prompt
 
-/* add loop and other code here ... in this simple exercise we are not
-   going to concern ourselves with minimizing globals, etc */
+function loadTable(table){
+    const tableBody = table.querySelector("tbody");
 
-function loop(){
-    for (let i = 0; i < 3; i++) {
-        let product = cart[i].product;
-        let price = product.price;
-        let quantity = cart[i].quantity;
-        let total = calculateTotal(price, quantity);
-        outputCartRow(product, total);
-        console.log(total);
-        console.log(product);
+    //Clear table to use dynamic instead
+    tableBody.innerHTML = "<tr></tr>";
+
+    //Fill in rows
+    let subtotal = 0;
+    for(let i = 0; i < 3; i++){
+        const row = document.createElement("tr");               //Create row
+        const img = document.createElement('img');              //Create image
+        img.src = "images/"+cart[i].product.filename;                    //Add image
+        row.appendChild(img);
+
+        //Create title, quantity and price
+        createContent(row, cart[i].product.title);
+        createContent(row, cart[i].quantity);
+        createContent(row, cart[i].product.price);
+
+        //Create total
+        let total = parseInt(calculateTotal(cart[i].quantity, cart[i].product.price));
+        subtotal += total;
+        total = moneyFormat(total)
+        createContent(row, total);
+
+        tableBody.append(row);                                          //Add row to body
     }
+
+    //Calculate and show subtotal, tax, shipping and grand total
+    addRow(tableBody,"totals", 4, "Subtotal", subtotal, false);                     //Show subtotal
+    const tax = tax_rate*subtotal;
+    addRow(tableBody,"totals", 4, "Tax", tax, false);                               //Show tax
+    const shipping = subtotal > shipping_threshold ? 0 : 40;
+    addRow(tableBody,"totals", 4, "Shipping", shipping, false);                     //Show shipping
+    addRow(tableBody,"totals", 4, "Grand Total", subtotal+tax+shipping, true); //GrandTotal
 }
+
+loadTable(document.querySelector("table.table-fill"));
